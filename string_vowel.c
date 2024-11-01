a7f7b4186ba3dbd50f6860186ba622e8925c7be3
mockingjay777 <ishikaacharya123@gmail.com>
2024-11-01 18:32:00 +0530

    Input method in counting vowels code modified

--- string_vowel.c
+++ string_vowel.c
@@ -2,7 +2,7 @@
 int main(){
 	char str[100];
 	printf("Enter string\n");
-	scanf("%s",str);
+	fgets(str,100,stdin);
 	int count=0,i=0;
 	while(str[i]!='\0'){
 		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
