7d757bfe1a30c39f2c618aee488b7e25bfb121ce
mockingjay777 <ishikaacharya123@gmail.com>
2024-11-01 19:26:36 +0530

    Convert to lower case without using functions: input method and condition modified

--- str_lower.c
+++ str_lower.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 int main(){
 	char str1[100];
 	printf("Enter string\n");
-	scanf("%s",str1);
-	int length=strlen(str1);
+	fgets(str1,100,stdin);
+	int length=0;
+	while(str1[length]!='\0'){
+		length++;
+	}
 	char lower[length];
 	for(int i=0; i<=length; i++){
-		if(str1[i]>='a'){
-			lower[i]=str1[i];
-		}else{
+		if((str1[i]>='A')&&(str1[i]<='Z')){
 			lower[i]=str1[i]+32;
+		}else{
+			lower[i]=str1[i];
 		}
 	}	
 	printf("String in lowercase: %s\n",lower);
