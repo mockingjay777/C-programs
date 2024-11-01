a8fcbac6f849d607637d4362a13f36fcff429d85
mockingjay777 <ishikaacharya123@gmail.com>
2024-11-01 19:22:08 +0530

    Convert string to lowercase using library functions

--- str_lower2.c
+++ str_lower2.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 int main(){
 	char str1[100];
         printf("Enter string\n");
-        scanf("%s",str1);
+        fgets(str1,100,stdin);
 	int length=0;
-	while(str1[length]!='\0'){
-		length++;
-	}char lower[length];
+	length=strlen(str1);
         for(int i=0; i<=length; i++){
-                if(str1[i]>='a'||str1[i]=='\0'){
-                        lower[i]=str1[i];
-                }else{
-                        lower[i]=str1[i]+32;
-                }
-        }
-        printf("String in lowercase: %s\n",lower);
+                str1[i]=tolower(str1[i]);
+	}
+        printf("String in lowercase: %s\n",str1);
         return 0;
 }
