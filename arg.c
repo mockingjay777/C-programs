e765131da28db4716f243b7c0ddee818abcfa5ce
mockingjay777 <ishikaacharya123@gmail.com>
2024-11-08 12:08:10 +0530

    Second function

---
+++ arg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void add(int a,int b);
+
+int main(){
+	int x=5,y=10;
+	add(x,y);
+	return 0;
+}
+
+void add(int a,int b){
+	int ans=a+b;
+	printf("%d + %d = %d \n",a,b,ans);
+}
